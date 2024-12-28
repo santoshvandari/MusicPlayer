d3d4cd08cd31fa6337e714b29d2c6e79e284aa5b
Santosh Bhandari <santoshvandari1@gmail.com>
2024-12-28 17:02:13 +0545

    Background playing features Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
-import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:music/screens/home_screen.dart';
-import 'package:music/services/audio_handler.dart';
 
-late final AudioHandler audioHandler;
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  audioHandler = await AudioService.init(
-    builder: () => MyAudioHandler(),
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.example.musicplayer.channel.audio',
-      androidNotificationChannelName: 'Music Player',
-      androidNotificationOngoing: true,
-    ),
-  );
+void main() {
   runApp(const MusicPlayer());
 }
 
