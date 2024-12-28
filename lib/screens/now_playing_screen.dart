7d9d6af1a2ff389b44eaa705e19cd7bf14a352d6
Santosh Bhandari <santoshvandari1@gmail.com>
2024-12-28 17:42:18 +0545

    Added the Play Next, PRevious, Suffle and repeat Features

--- lib/screens/now_playing_screen.dart
+++ lib/screens/now_playing_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:audio_service/audio_service.dart';
 
 class NowPlayingScreen extends StatefulWidget {
   final List<String> songs;
